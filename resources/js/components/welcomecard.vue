b896ba86ca015937489b18a2ca23cc4de15ade8f
tanawitps <tanawit.ps@gmail.com>
2020-03-05 20:43:14 +0700

    [update] responsive,navbar welcome

--- resources/js/components/welcomecard.vue
+++ resources/js/components/welcomecard.vue
@@ -1,14 +1,14 @@
 <template>
-    <v-card class="mx-auto p-4" style="max-width:80vw; height:auto; max-height:100vh;">
+    <v-card class="mx-auto p-4" style="max-width:80vw; max-height:90vh;">
         <v-card-text>
             <div
-                style="text-align:center;"
-                class="font-weight-bold text--primary headtext"
+                style="text-align:center; font-size: calc(1em + 1vw);"
+                class="font-weight-bold text--primary"
             >
                 Lab Lender
             </div>
 
-            <div class="mt-4 mb-3 headsubtext" style="text-align:center;">
+            <div class="mt-4 mb-3 headsubtext" style="text-align:center; font-size: calc(0.75em + 0.75vw);">
                 web application that help you find and lend all equipments in
                 the laboratory
             </div>
@@ -23,13 +23,11 @@
                 />
             </center>
             <center>
-        
         <v-btn
             style="text-decoration: none; color : #FFFFFF;"
             href="/login"
             class="button-btn-gradient"
             large
-
         >
             LOGIN WITH CMU ACCOUNT
         </v-btn></center>
@@ -49,21 +47,21 @@ div.headtext {
 
 @media screen and (min-width: 801px) {
     div.headtext {
-        font-size: 2.25vw;
+        font-size: calc(1em + 1vw);
     }
 }
-
-@media screen and (max-width: 800px) {
+/* font-size: 2.25em; */
+/* @media screen and (max-width: 800px) {
     div.headtext {
         font-size: 6vw;
     }
-}
+} */
 
 div.headsubtext {
     color: "black";
 }
 
-@media screen and (min-width: 801px) {
+@media screen and (min-width: 801px){
     div.headsubtext {
         font-size: 1.25vw;
     }
--- resources/js/pages/mainhome/About.vue
+++ resources/js/pages/mainhome/About.vue
@@ -1,11 +1,19 @@
 <template>
     <div id="app">
         <v-app id="inspire">
-            <v-card class="mt-4 elevation-4 d-inline-block mx-auto">
+            <v-card class="mt-4 elevation-4 d-inline-block mx-auto" style="max-width:80vw; max-height:90vh;">
+                <div class="flex-container">
                 <v-container>
                     <v-row justify="space-between">
                         <v-col cols="auto">
-                            <v-img class="ml-3" src="/img/cpe.png"></v-img>
+                            <center>
+                            <v-img
+                             class="ml-3 align-center" 
+                             src="/img/cpe.png"
+                             max-width="80%"
+                            max-height="80%"
+                             ></v-img>
+                            </center>
                         </v-col>
 
                         <v-col cols="auto" class="text-center pl-0">
@@ -13,7 +21,7 @@
                                 class="flex-column ma-0 fill-height"
                                 justify="center"
                             >
-                                <v-col style="font-size: 1vw; font-family: 'Kanit' , sans-serif;" align="center" class="mt-7 mx-3">
+                                <v-col style="font-size: calc(0.6em + 0.6vw); font-family: 'Kanit' , sans-serif;" align="center" class="mt-7 mx-3">
                                     เว็บไซต์นี้พัฒนาโดยนักศึกษาวิชาภาควิชาวิศวกรรมคอมพิวเตอร์
                                     มหาวิทยาลัยเชียงใหม่ (CPE#25)<br />
                                     ภาควิชาวิศวกรรมคอมพิวเตอร์ ชั้น 4 ตึก 30 ปี
@@ -28,14 +36,28 @@
                         </v-col>
                     </v-row>
                 </v-container>
+                </div>
             </v-card>
         </v-app>
     </div>
 </template>
 
 <style scoped>
-.headfont{
 
+div.flex-container {
+    display: flex;
+}
+
+@media screen and (min-width: 961px) {
+    div.flex-container {
+        flex-direction: row;
+    }
+}
+
+@media screen and (max-width: 960px) {
+    div.flex-container {
+        flex-direction: column;
+    }
 }
 
 </style>
