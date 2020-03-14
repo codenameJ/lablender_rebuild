9378617cbb757b5343ec296bdadb60857c773a7e
tanawitps <tanawit.ps@gmail.com>
2020-03-14 19:27:45 +0700

    commit

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,6 +1,6 @@
 import createPersistedState from 'vuex-persistedstate'
 
-export default{
+export default {
     state: {
         users: [],
         labs: [],
@@ -9,50 +9,50 @@ export default{
     },
     plugins: [createPersistedState()],
     mutations: {
-        SetLabs(state, labs){
-            state.labs=labs;
+        SetLabs(state, labs) {
+            state.labs = labs;
         },
-        SetUsers(state, users){
-            state.users=users;
+        SetUsers(state, users) {
+            state.users = users;
         },
-        SetCurrentLab(state, selectedLab){
-            state.selectedLab=selectedLab;
+        SetCurrentLab(state, selectedLab) {
+            state.selectedLab = selectedLab;
         },
-        SetCurrentUser(state, selectedUser){
-            state.selectedUser=selectedUser;
+        SetCurrentUser(state, selectedUser) {
+            state.selectedUser = selectedUser;
         },
     },
     getters: {
         getcurrentlab: (state) => (course_id) => {
             return state.labs.find(lab => lab.course_id === course_id)
-          },
+        },
 
         professerlab: state => {
             return state.labs.filter(lab => lab.professor_name)
-          },
+        },
         getUser(state) {
             return state.users;
         },
     },
     actions: {
-        loadLabs({commit}){
+        loadLabs({ commit }) {
             axios.get("/api/lab").then(data => {
                 // console.log(data.data)
-            let labs = data.data
-            commit('SetLabs', labs)
-        })
+                let labs = data.data
+                commit('SetLabs', labs)
+            })
         },
-        loadUsers({commit}){
+        loadUsers({ commit }) {
             axios.get("/api/user").then(data => {
                 console.log(data.data)
-            let users = data.data
-            commit('SetUsers', users)
-        })
+                let users = data.data
+                commit('SetUsers', users)
+            })
         },
-        currentLab({commit}, curlab){
+        currentLab({ commit }, curlab) {
             commit('SetCurrentLab', curlab)
         },
-        currentUser({commit}, curuser){
+        currentUser({ commit }, curuser) {
             commit('SetCurrentUser', curuser)
         },
     },
